0713aaa2792a561e833af9f363aff5b9459021e8
Illinois_Roadbuff <fiel.rodriguez.jr@gmail.com>
2025-07-04 14:47:06 -0500

    add another easter egg
    Signed-off-by: Illinois_Roadbuff <fiel.rodriguez.jr@gmail.com>

---
+++ app/pages/xDwgzf/jyts/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const flashRed = ref(false)
+const showScare = ref(false)
+const show404 = ref(false)
+
+onMounted(() => {
+  const query = new URLSearchParams(window.location.search)
+  if (query.has('knockknock')) {
+    // Flash red then show scare
+    let flashCount = 0
+    const maxFlashes = 6
+    const interval = setInterval(() => {
+      flashRed.value = !flashRed.value
+      flashCount++
+      if (flashCount >= maxFlashes) {
+        clearInterval(interval)
+        flashRed.value = false
+        showScare.value = true
+      }
+    }, 100)
+  } else {
+    show404.value = true
+  }
+})
+</script>
+
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center transition-colors duration-100 text-white"
+    :class="{
+      'bg-red-900': flashRed,
+      'bg-black': !flashRed
+    }"
+  >
+    <div v-if="show404" class="text-center">
+      <h1 class="text-6xl font-bold">404</h1>
+      <p class="text-xl mt-2">This page could not be found.</p>
+    </div>
+
+    <div v-else-if="showScare" class="text-center">
+      <h1 class="text-5xl font-bold">ASDIWDHSDWSkADHSKHDKSASJAFIUAWYHEWIR7r7786DS^&ATD^ASDASyuagdkSADSABDHAYWDSUAKBSABDCHJKDADSGDAWDADAWDadihsa2fskJKJADSABDJASDSADSAJDKABDkSABDSAKDASBKDSABADSBAD</h1>
+      <p class="mt-4">YOU WILL NOW SUFFER</p>
+    </div>
+  </div>
+</template>
