6b4893ca7975ab321d8cb2f49f030d1b3eb6708e
Illinois_Roadbuff <133445590+illinois-roadbuff@users.noreply.github.com>
2025-11-21 02:08:21 -0600

    Update index.vue
    add CC info

--- app/pages/to/index.vue
+++ app/pages/to/index.vue
@@ -3,7 +3,7 @@ import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 
-type SocialKey = 'discord' | 'github' | 'youtube' | 'codeberg' | 'guilded' | 'apple' | 'robloxdev' | 'roblox' | 'wikipedia' | 'bandcamp' 
+type SocialKey = 'discord' | 'github' | 'youtube' | 'codeberg' | 'guilded' | 'apple' | 'robloxdev' | 'roblox' | 'wikipedia' | 'bandcamp' | 'ccinfoYT_SA'
 
 const socials: Record<SocialKey, string> = {
     github: 'https://github.com/illinois-roadbuff',
@@ -18,6 +18,7 @@ const socials: Record<SocialKey, string> = {
    bandcamp: 'https://illinois-roadbuff.bandcamp.com/',
 
    roblox: 'https://www.roblox.com/users/2653606809/profile',
+   ccinfoYT_SA: 'https://docs.google.com/document/d/1VLhwc8GuXSwWcvJ3ZNYKTFqPnW7jlb_i8ZVz8w14lto/edit?usp=sharing',
  
   
  
